d077dac614910d3b9dc347f2f0fd6ed32b26edee
Matija Jaic <jaicmatija@gmail.com>
2019-03-26 11:43:58 +0100

    Initialize node serrver

--- server.js
+++ server.js
@@ -1,58 +1,6 @@
+const http = require('http');
 
-
-const halls = [{
-    id: 234,
-    name: "Dvorana Gradske knjižnice",
-    reservation: {
-        isReserved: true,
-        reservedFrom: new Date(2019, 3, 25, 10),
-        reservedUntil: new Date(2019, 3, 25, 12),
-    }
-}, {
-    id: 264,
-    name: "Konferencijske dvorane RCTP (Razvojni centar i tehnološki park)",
-    reservation: {
-        isReserved: false,
-        reservedFrom: null,
-        reservedUntil: null,
-    }
-}, {
-    id: 534,
-    name: "Dvorana TIC (Turistički dom)",
-    reservation: {
-        isReserved: false,
-        reservedFrom: null,
-        reservedUntil: null,
-    }
-}, {
-    id: 123,
-    name: "Dvorana Hrvatskog doma",
-    reservation: {
-        isReserved: true,
-        reservedFrom: new Date(2019, 3, 23, 12),
-        reservedUntil: new Date(2019, 3, 23, 14),
-    }
-}, {
-    id: 425,
-    name: "Velika sportska dvorana",
-    reservation: {
-        isReserved: true,
-        reservedFrom: new Date(2019, 3, 25, 11),
-        reservedUntil: new Date(2019, 3, 25, 12),
-    }
-}];
-
-const hallNames = halls.map(hall => hall.name);
-
-const hallNamesWithComma = hallNames.join(', ');
-
-console.log(hallNamesWithComma);
-
-
-
-
-
-
-
-
-
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080);
