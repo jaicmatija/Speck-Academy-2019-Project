fc09d774aeb8b3823beec69c577ccd115d83771d
Matija Jaic <jaicmatija@gmail.com>
2019-03-27 21:58:49 +0100

    Add reservationWeekday response

--- reservationWeekday.js
+++ reservationWeekday.js
@@ -1,6 +1,13 @@
 const http = require('http');
+const { halls } = require('./halls');
+const { DateTime } = require('luxon');
+
+const reservedHalls = halls.filter(hall => hall.reservation.isReserved);
+const firstReservedHall = reservedHalls[0];
+const reservedUntilDateTime = DateTime.fromJSDate(firstReservedHall.reservation.reservedUntil);
+
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end(reservedUntilDateTime.toFormat('cccc,LLLL dd'));
 }).listen(8080);
