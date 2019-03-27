2527af74b7aaa4a2adf30922a5fff97871bc6cbf
Matija Jaic <jaicmatija@gmail.com>
2019-03-27 19:28:41 +0100

    Create halls.js

---
+++ halls.js
@@ -0,0 +1,45 @@
+const halls = [{
+    id: 234,
+    name: "Dvorana Gradske knjižnice",
+    reservation: {
+        isReserved: true,
+        reservedFrom: new Date(2019, 3, 25, 10),
+        reservedUntil: new Date(2019, 3, 25, 12),
+    }
+}, {
+    id: 264,
+    name: "Konferencijske dvorane RCTP (Razvojni centar i tehnološki park)",
+    reservation: {
+        isReserved: false,
+        reservedFrom: null,
+        reservedUntil: null,
+    }
+}, {
+    id: 534,
+    name: "Dvorana TIC (Turistički dom)",
+    reservation: {
+        isReserved: false,
+        reservedFrom: null,
+        reservedUntil: null,
+    }
+}, {
+    id: 123,
+    name: "Dvorana Hrvatskog doma",
+    reservation: {
+        isReserved: true,
+        reservedFrom: new Date(2019, 3, 23, 12),
+        reservedUntil: new Date(2019, 3, 23, 14),
+    }
+}, {
+    id: 425,
+    name: "Velika sportska dvorana",
+    reservation: {
+        isReserved: true,
+        reservedFrom: new Date(2019, 3, 25, 11),
+        reservedUntil: new Date(2019, 3, 25, 12),
+    }
+}];
+
+module.exports = {
+    halls
+};
