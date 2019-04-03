fccb641499a4b3ba6fc63faa96c4eb68b732cd6d
Matija Jaic <jaicmatija@gmail.com>
2019-04-03 23:17:11 +0200

    Serve static files on root path

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
-const { halls } = require('halls');
+const { halls } = require('./halls');
+
+app.use("/", express.static('public'));
 
 app.get("/halls", function(req, res) {
     res.json(halls);    
 });
+
+app.listen(3000);
+
+
